805a324be9060e3759283de5b875009111a4c724
Rajprudvin <prudvirajn@gmail.com>
2024-09-25 19:18:05 +0530

    vpc-module-test

--- variables.tf
+++ variables.tf
@@ -52,7 +52,6 @@ variable "private_subnet_tags" {
     default = {}
 }
 
-
 variable "database_subnet_cidrs" {
     type = list
     validation {
@@ -71,6 +70,27 @@ variable "db_subnet_group_tags" {
 
 variable "nat_gateway_tags" {
     default = {}
+}
+
+variable "public_route_table_tags" {
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    default = {}
+}
+
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+
+variable "vpc_peering_tags" {
+    default ={}
 }
 
 
